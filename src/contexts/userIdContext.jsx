523fdf94d4295aa13e1cf60cbfcc533d95eb5b81
Judith Bohmann <judith.bohmann@dci-student.org>
2025-06-04 13:37:35 +0200

    lots of try-and-error and a beautifull context is born

---
+++ src/contexts/userIdContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect } from "react";
+
+// JB: Create a context
+export const UserContext = createContext();
+
+// JB: Create a provider for the userId context
+export const UserProvider = ({ children }) => {
+
+
+    const [userId, setUserId] = useState(() => {
+      // JB: search for the userId inside the localStorage
+      return localStorage.getItem("userId") || null;
+    });
+  
+    // JB: If the browser finds a userId it will store it in the localStorage ... everytime when the userId changes (dependecy of the useEffect)
+    useEffect(() => {
+      if (userId) {
+        localStorage.setItem("userId", userId);
+      }
+    }, [userId]);
+
+  return (
+    <UserContext.Provider value={{ userId, setUserId }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
