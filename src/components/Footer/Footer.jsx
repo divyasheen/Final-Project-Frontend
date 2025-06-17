3f982da23a6507c567d516c1c458dce7ed73ec1d
Avdyl <avdyl.zekaj@dci-student.org>
2025-06-17 18:33:23 +0200

    footer colors

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,15 @@
-import "./_footer.scss"
+import "./_footer.scss";
 
 function Footer() {
+  const isChatActive =
+    typeof document !== "undefined" &&
+    document.body.classList.contains("chat-active");
 
-  const isChatActive = typeof document !== "undefined" && document.body.classList.contains("chat-active")
-
-  if (isChatActive) return null
+  if (isChatActive) return null;
 
   return (
     <>
-      <footer className="sticky top-[100vh] bg-gradient-to-r from-indigo-900 to-fuchsia-900 py-4 text-center border-t border-secondary">
+      <footer className="sticky top-[100vh] bg-[linear-gradient(220deg,#cf82e4_0%,rgba(78,50,235,0.9)_70%,rgba(78,50,235,0.3)_100%)] py-4 text-center border-t border-secondary">
         <p className="text-white text-sm">Impressum</p>
       </footer>
     </>
