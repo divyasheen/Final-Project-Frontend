44938e487404541837cedb2ad2f4077fa078bc94
Judith Bohmann <judith.bohmann@dci-student.org>
2025-06-17 12:20:09 +0200

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "oklch(12.9% 0.042 264.695)",            // General dark background
-        primary: "oklch(40.1% 0.17 325.612)",               // Used for footer, navbar background
-        secondary: "oklch(48.8% 0.243 264.376)",             // Main accent (buttons, XP highlights)
+        background: "#0b0225",            // General dark background
+        primary: "#832A9B",               // Used for footer, navbar background
+        secondary: "#210187",             // Main accent (buttons, XP highlights)
         secondaryHover: "#45c0db",        // Hover effect for secondary
         surface: "#1E1E2F",               // Panels or card backgrounds
         codeBlock: "oklch(53.2% 0.157 131.589)",             // For code snippets and console background
         glow: "rgba(87, 224, 254, 0.3)",  // Subtle hover glow
-        gradientToR: "bg-gradient-to-r from-fuchsia-900 to-indigo-900", // gradient primary to r
-
-        gradientToL: "linear-gradient(90deg,rgba(131, 42, 155, 1) 0%, rgba(28, 6, 99, 1) 100%)",
       },
       fontFamily: {
         vt323: ['"VT323"', "monospace"],
